c45856873d44d067013b1e58507c4b14d2bc2ba8
Jason Vargas <javarg410@knights.ucf .edu>
2019-07-18 18:17:33 -0400

    Added Audio to the game and attatched it to the player object

--- Tutorial2.2(Platformer)/Assets/Scripts/PlayerController.cs
+++ Tutorial2.2(Platformer)/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,45 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody2D rb2d;
+    //====== Public Variables ======//
+
     public float speed;
     public float jumpForce;
     public Text countText;
     public Text winText;
     public Text livesText;
 
+    public AudioClip levelMusic;
+    public AudioClip winMusic;
+    public AudioSource musicSource;
+
+    //===============================//
+
+    //====== Private Variables ======//
+
+    private Rigidbody2D rb2d;
     private int count;
     private int lives;
+
+    //===============================//
     
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        musicSource.clip = levelMusic;
+        musicSource.Play();
+
+        musicSource.loop = true;
+
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
         lives = 3;
         winText.text = "";
 
         SetAllText();
+
     }
 
     // Update is called once per frame
@@ -36,11 +55,20 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        //================ Play Music ===================//
+        
+        
+
+        //================================================//
+
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         Vector2 movement = new Vector2(moveHorizontal, 0);
 
         rb2d.AddForce(movement * speed);
+
+        //================================================//
         
         if (Input.GetKey("escape"))Application.Quit();
 
@@ -52,10 +80,14 @@ public class PlayerController : MonoBehaviour
 
         }
 
+        //================================================//
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+
+        //=========== Collision Detection ===============//
+
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
@@ -73,6 +105,8 @@ public class PlayerController : MonoBehaviour
             SetAllText();
 
         }
+
+        //=============== Check Score =================//
         
         if(count == 4)
         {
@@ -82,11 +116,20 @@ public class PlayerController : MonoBehaviour
             SetAllText();
         }
 
+
+                    //====== Victory Zone ======//
+
         if(count == 8)
         {
             winText.text = "You Win!";
+
+            musicSource.clip = winMusic;
+            musicSource.Play();
+
+            musicSource.loop = true;
         }
         
+        //================================================//
     }
 
     void OnCollisionStay2D(Collision2D collision)
